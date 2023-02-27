36b244c5b409fa00a99977998bf88e5352c06b4e
hilcj0001@gmail.com <hilcj0001@gmail.com>
2023-02-26 21:06:15 -0800

    first commit

---
+++ sync-inputs-google.js
@@ -0,0 +1,62 @@
+// import { kPublisherGoogleSearch, kMethodUpdateText, kSubmit, subscribeRuntimeMessages } from "sync-inputs-utils.js";
+
+// Utils
+function getTextArea() {
+    var t = Array.from(document.getElementsByName("q")).find(e => e.type == "text");
+    if (!t) {
+        console.warn("Google search inputs not found.");
+    }
+    return t;
+}
+
+function getSearchButtons() {
+    var buttons = Array.from(document.getElementsByTagName("input")).filter(b => b.ariaLabel == "Google Search" && b.role == "button");
+    buttons = buttons.concat(Array.from(document.getElementsByTagName("button")).filter(b => b.ariaLabel == "Search"));
+    if (buttons.length == 0) {
+        console.warn("Google search button not found.");
+    }
+    return buttons;
+}
+
+// Methods
+function updateText(text) {
+    getTextArea().value = text;
+}
+
+function submit() {
+    var buttons = getSearchButtons();
+    buttons[0].click();
+}
+
+function registerRuntimeMessagePublisher() {
+    var q = getTextArea();
+    q.addEventListener("input", function (e) {
+        chrome.runtime.sendMessage({
+            publisher: kPublisherGoogleSearch,
+            method: kMethodUpdateText,
+            text: e.target.value
+        });
+    });
+    q.addEventListener("keydown", function (e) {
+        if (e.key == "Enter") {
+            chrome.runtime.sendMessage({
+                publisher: kPublisherGoogleSearch,
+                method: kMethodSubmit
+            });
+        }
+    });
+    getSearchButtons().forEach(button => button.addEventListener("mousedown", function (e) {
+        chrome.runtime.sendMessage({
+            publisher: kPublisherGoogleSearch,
+            method: kMethodSubmit
+        });
+    }
+    ));
+}
+
+
+// Register publisher and subscriber.
+subscribeRuntimeMessages(kPublisherGoogleSearch, updateText, submit);
+
+registerRuntimeMessagePublisher();
+document.body.addEventListener("click", registerRuntimeMessagePublisher);
---
+++ sync-inputs-openai.js
@@ -0,0 +1,62 @@
+// import { kPublisherChatGPT, kMethodUpdateText, kSubmit, subscribeRuntimeMessages } from "sync-inputs-utils.js";
+
+// Utils
+function getTextArea() {
+    var textareas = document.getElementsByTagName("textarea");
+    if (textareas.length == 0) {
+        console.warn("Chatgpt inputs not found.");
+    } else if (textareas.length > 1) {
+        console.warn("Chatgpt inputs unexpected.");
+    }
+    return textareas[0];
+}
+
+function getSubmitButton() {
+    var polygons = Array.from(document.getElementsByTagName("polygon")).filter(p => p.parentElement.parentElement.type == "submit");
+    if (polygons.length == 0) {
+        console.warn("Chatgpt button not found.");
+    } else if (polygons.length > 1) {
+        console.warn("Chatgpt inputs unexpected.");
+    }
+    return polygons[0].parentElement.parentElement;
+}
+
+// Methods
+function updateText(text) {
+    getTextArea().value = text;
+}
+
+function submit() {
+    getSubmitButton().click();
+}
+
+function registerRuntimeMessagePublisher() {
+    var t = getTextArea();
+    t.addEventListener("input", function (e) {
+        chrome.runtime.sendMessage({
+            publisher: kPublisherChatGPT,
+            method: kMethodUpdateText,
+            text: e.target.value
+        });
+    });
+    t.addEventListener("keydown", function (e) {
+        if (e.key == "Enter") {
+            chrome.runtime.sendMessage({
+                publisher: kPublisherChatGPT,
+                method: kMethodSubmit
+            });
+        }
+    });
+    getSubmitButton().addEventListener("click", function (e) {
+        chrome.runtime.sendMessage({
+            publisher: kPublisherChatGPT,
+            method: kMethodSubmit
+        });
+    });
+}
+
+// Register publisher and subscriber.
+subscribeRuntimeMessages(kPublisherChatGPT, updateText, submit);
+
+registerRuntimeMessagePublisher();
+document.body.addEventListener("click", registerRuntimeMessagePublisher);
